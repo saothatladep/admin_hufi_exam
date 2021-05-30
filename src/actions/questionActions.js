a390da720d99fe00da37d4db489eda19bbc02430
Pham Toan <Pham Toan@PHAMTOAN>
2021-05-30 18:22:44 +0700

    commit update lastest

--- src/actions/questionActions.js
+++ src/actions/questionActions.js
@@ -1,25 +1,25 @@
 import {
-    QUESTION_CREATE_FAIL,
-    QUESTION_CREATE_REQUEST,
-    QUESTION_CREATE_RESET,
-    QUESTION_CREATE_SUCCESS,
-    QUESTION_DELETE_FAIL,
-    QUESTION_DELETE_REQUEST,
-    QUESTION_DELETE_SUCCESS,
-    QUESTION_DETAILS_FAIL,
-    QUESTION_DETAILS_REQUEST,
-    QUESTION_DETAILS_RESET,
-    QUESTION_DETAILS_SUCCESS,
-    QUESTION_LIST_FAIL,
-    QUESTION_LIST_REQUEST,
-    QUESTION_LIST_SUCCESS,
-    QUESTION_UPDATE_FAIL,
-    QUESTION_UPDATE_REQUEST,
-    QUESTION_UPDATE_RESET,
-    QUESTION_UPDATE_SUCCESS,
-  } from '../constants/questionConstants'
-  import axios from 'axios'
-  
+  QUESTION_CREATE_FAIL,
+  QUESTION_CREATE_REQUEST,
+  QUESTION_CREATE_RESET,
+  QUESTION_CREATE_SUCCESS,
+  QUESTION_DELETE_FAIL,
+  QUESTION_DELETE_REQUEST,
+  QUESTION_DELETE_SUCCESS,
+  QUESTION_DETAILS_FAIL,
+  QUESTION_DETAILS_REQUEST,
+  QUESTION_DETAILS_RESET,
+  QUESTION_DETAILS_SUCCESS,
+  QUESTION_LIST_FAIL,
+  QUESTION_LIST_REQUEST,
+  QUESTION_LIST_SUCCESS,
+  QUESTION_UPDATE_FAIL,
+  QUESTION_UPDATE_REQUEST,
+  QUESTION_UPDATE_RESET,
+  QUESTION_UPDATE_SUCCESS,
+} from '../constants/questionConstants';
+import axios from 'axios';
+
 //   export const listChapter = (id, keyword = '', pageNumber = '') => async (
 //     dispatch,
 //     getState
@@ -28,23 +28,23 @@ import {
 //       dispatch({
 //         type: QUESTION_LIST_REQUEST,
 //       })
-  
+
 //       const {
 //         userLogin: { userInfo },
 //       } = getState()
-  
+
 //       const config = {
 //         headers: {
 //           'Content-Type': 'application/json',
 //           Authorization: `Bearer ${userInfo.token}`,
 //         },
 //       }
-  
+
 //       const { data } = await axios.get(
 //         `/api/chapters/subject/${id}?keyword=${keyword}&pageNumber=${pageNumber}`,
 //         config
 //       )
-  
+
 //       dispatch({
 //         type: QUESTION_LIST_SUCCESS,
 //         payload: data,
@@ -59,40 +59,37 @@ import {
 //       })
 //     }
 //   }
-  
-  export const createQuestion = (question) => async (dispatch, getState) => {
-    try {
-      dispatch({
-        type: QUESTION_CREATE_REQUEST,
-      })
-  
-      const {
-        userLogin: { userInfo },
-      } = getState()
-  
-      const config = {
-        headers: {
-          Authorization: `Bearer ${userInfo.token}`,
-        },
-      }
-  
-      const { data } = await axios.post(`/api/questions`, question, config)
-  
-      dispatch({
-        type: QUESTION_CREATE_SUCCESS,
-        payload: data,
-      })
-    } catch (error) {
-      dispatch({
-        type: QUESTION_CREATE_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      })
-    }
+
+export const createQuestion = (question) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: QUESTION_CREATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.post(`/api/questions`, question, config);
+
+    dispatch({
+      type: QUESTION_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: QUESTION_CREATE_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+    });
   }
-  
+};
+
 //   export const listChapterDetails = (id) => async (dispatch) => {
 //     try {
 //       dispatch({ type: QUESTION_DETAILS_REQUEST })
@@ -108,30 +105,30 @@ import {
 //       })
 //     }
 //   }
-  
+
 //   export const updateChapter = (chapter) => async (dispatch, getState) => {
 //     try {
 //       dispatch({
 //         type: QUESTION_UPDATE_REQUEST,
 //       })
-  
+
 //       const {
 //         userLogin: { userInfo },
 //       } = getState()
-  
+
 //       const config = {
 //         headers: {
 //           'Content-Type': 'application/json',
 //           Authorization: `Bearer ${userInfo.token}`,
 //         },
 //       }
-  
+
 //       const { data } = await axios.put(
 //         `/api/chapters/${chapter._id}`,
 //         chapter,
 //         config
 //       )
-  
+
 //       dispatch({
 //         type: QUESTION_UPDATE_SUCCESS,
 //         payload: data,
@@ -146,25 +143,25 @@ import {
 //       })
 //     }
 //   }
-  
+
 //   export const deleteChapter = (id) => async (dispatch, getState) => {
 //     try {
 //       dispatch({
 //         type: QUESTION_DELETE_REQUEST,
 //       })
-  
+
 //       const {
 //         userLogin: { userInfo },
 //       } = getState()
-  
+
 //       const config = {
 //         headers: {
 //           Authorization: `Bearer ${userInfo.token}`,
 //         },
 //       }
-  
+
 //       await axios.delete(`/api/chapters/${id}`, config)
-  
+
 //       dispatch({
 //         type: QUESTION_DELETE_SUCCESS,
 //       })
@@ -178,4 +175,3 @@ import {
 //       })
 //     }
 //   }
-  
